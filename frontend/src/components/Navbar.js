9c11b6b3f72305138d382622126a1e4e8625d97b
Abdul Majeed Sualihu <abdulmajeedsualihu2000@gmail.com>
2025-08-03 05:15:04 +0000

    frontend components/Navbar.js

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const CustomNavbar = () => {
+  return (
+    <Navbar bg="success" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand as={Link} to="/">FarmDirect</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/farmers">Farmers</Nav.Link>
+            <Nav.Link as={Link} to="/products">Products</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+            <Nav.Link as={Link} to="/register">Register</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default CustomNavbar;
