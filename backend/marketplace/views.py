3279113a25ffd7093a9eabc66a7801c3c65b4617
Abdul Majeed Sualihu <abdulmajeedsualihu2000@gmail.com>
2025-08-03 05:08:07 +0000

    Farmers and product List

--- backend/marketplace/views.py
+++ backend/marketplace/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import FarmerProfile, Product
+from .serializers import FarmerProfileSerializer, ProductSerializer
 
-# Create your views here.
+class FarmerList(generics.ListCreateAPIView):
+    queryset = FarmerProfile.objects.all()
+    serializer_class = FarmerProfileSerializer
+
+class FarmerDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = FarmerProfile.objects.all()
+    serializer_class = FarmerProfileSerializer
+
+class ProductList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
